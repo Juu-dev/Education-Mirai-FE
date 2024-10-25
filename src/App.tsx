8bdb75a1439825e90ec86834231935b01306f313
Patrick_Hieu <hieunguyenthanh204@gmail.com>
2024-10-25 10:32:53 +0700

    Fixed LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { RoleProvider } from './context/RoleContext';
-import Login from './pages/auth/LoginPage';
 import Unauthorized from './pages/auth/Unauthorized';
 import ProtectedRoute from './components/route/ProtectedRoute';
 import { Role } from './constants/roles/routes';
@@ -22,51 +21,52 @@ import AdminDocumentPage from './pages/admin/AdminDocumentPage';
 import LibraryStatisticalPage from './pages/librarian/LibraryStatisticalPage';
 import LibrarianDocumentPage from './pages/librarian/LibrarianDocumentPage';
 import { LibrarianAddBookPage } from './pages/librarian/LibrarianAddBookPage';
+import LoginPage from './pages/auth/LoginPage';
+
 
 const App: React.FC = () => {
-  return (
-    <RoleProvider>
-		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/unauthorized" element={<Unauthorized />} />
-
-			{/* Admin Routes */}
-			<Route element={<ProtectedRoute allowedRole={Role.Admin} />}>
-				<Route element={<AdminLayout />}>
-					<Route path="/admin" element={<AdminPage />} />
-					<Route path="/admin/page-one" element={<PageOne />} />
-					<Route path="/admin/dashboard" element={<AdminDashboard />} />
-					<Route path="/admin/class" element={<AdminClassPage />} />
-					<Route path="/admin/document" element={<AdminDocumentPage />} />
+	return (
+		<RoleProvider>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/unauthorized" element={<Unauthorized />} />
+				<Route path="/login" element={<LoginPage />} />
+				{/* Admin Routes */}
+				<Route element={<ProtectedRoute allowedRole={Role.Admin} />}>
+					<Route element={<AdminLayout />}>
+						<Route path="/admin" element={<AdminPage />} />
+						<Route path="/admin/page-one" element={<PageOne />} />
+						<Route path="/admin/dashboard" element={<AdminDashboard />} />
+						<Route path="/admin/class" element={<AdminClassPage />} />
+						<Route path="/admin/document" element={<AdminDocumentPage />} />
+					</Route>
 				</Route>
-			</Route>
 
-			{/* User Routes */}
-			<Route element={<ProtectedRoute allowedRole={Role.User} />}>
-				<Route element={<UserLayout />}>
-					<Route path="/user" element={<UserPage />} />
-					{/* <Route path="/user/page-one" element={<PageOne />} /> */}
-					<Route path="/user/books" element={<UserBookList />} />
-					<Route path='/user/reading-books' element={<UserBookReadingList />} />
-					<Route path="/user/reading-books/book/:id" element={<BookDetailPage />} />
-					<Route path="/user/assignments" element={<AssignmentPage />} />
-					<Route path="/user/assignments/:id" element={<AssignmentDetailPage />} />
+				{/* User Routes */}
+				<Route element={<ProtectedRoute allowedRole={Role.User} />}>
+					<Route element={<UserLayout />}>
+						<Route path="/user" element={<UserPage />} />
+						{/* <Route path="/user/page-one" element={<PageOne />} /> */}
+						<Route path="/user/books" element={<UserBookList />} />
+						<Route path='/user/reading-books' element={<UserBookReadingList />} />
+						<Route path="/user/reading-books/book/:id" element={<BookDetailPage />} />
+						<Route path="/user/assignments" element={<AssignmentPage />} />
+						<Route path="/user/assignments/:id" element={<AssignmentDetailPage />} />
+					</Route>
 				</Route>
-			</Route>
 
-			{/* Librarian Routes */}
-			<Route element={<ProtectedRoute allowedRole={Role.Librarian} />}>
-				<Route element={<AdminLayout />}>
-					<Route path="/librarian/dashboard" element={<AdminDashboard />} />
-					<Route path="/librarian/library" element={<LibraryStatisticalPage />} />
-					<Route path="/librarian/document" element={<LibrarianDocumentPage /> } />
-					<Route path="/librarian/books" element={<LibrarianAddBookPage /> } />
+				{/* Librarian Routes */}
+				<Route element={<ProtectedRoute allowedRole={Role.Librarian} />}>
+					<Route element={<AdminLayout />}>
+						<Route path="/librarian/dashboard" element={<AdminDashboard />} />
+						<Route path="/librarian/library" element={<LibraryStatisticalPage />} />
+						<Route path="/librarian/document" element={<LibrarianDocumentPage />} />
+						<Route path="/librarian/books" element={<LibrarianAddBookPage />} />
+					</Route>
 				</Route>
-			</Route>
-		</Routes>
-    </RoleProvider>
-  );
+			</Routes>
+		</RoleProvider>
+	);
 };
 
 export default App;
