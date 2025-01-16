12acb4f06642df840902e9c38be684e496daefe7
Juu-dev <thong.pm205029@sis.hust.edu.vn>
2025-01-16 23:22:19 +0700

    feature: filter document by class name

--- src/components/admin/documents/FolderTable.tsx
+++ src/components/admin/documents/FolderTable.tsx
@@ -7,6 +7,14 @@ interface Props {
     searchTerm: string;
 }
 
+const CLASSES = {
+    CLASS_ONE: ["1", "1A", "1B", "1C", "1D", "1E", "1G"],
+    CLASS_TWO_THREE: ["2A", "2B", "2C", "2D", "2E", "2G", "3", "3A", "3B", "3C", "3D", "3E"],
+    CLASS_FOUR_FIVE: ["4A", "4B", "4C", "4D", "4E", "4G", "5A", "5B", "5C", "5D", "5E", "5G"],
+    CLASS_SPECIAL: ["AN","MT","TH","TV","TD","TA"],
+    CLASS_PRINCIPAL: ["HT", "HP"]
+}
+
 const FolderTable = ({searchTerm}: Props) => {
     const navigate = useNavigate();
     const {data: users} = useFetchApi({url: `/users/librarian-and-teacher`, auth: true})
@@ -26,14 +34,13 @@ const FolderTable = ({searchTerm}: Props) => {
             dataIndex: "team",
             key: "team",
             filters: [
-                { text: "Tổ 1", value: 1 },
-                { text: "Tổ 2", value: 2 },
-                { text: "Tổ 3", value: 3 },
-                { text: "Tổ 4", value: 4 },
-                { text: "Tổ 5", value: 5 },
-                { text: "Tổ 6", value: 6 }
+                { text: "Tổ 1", value: CLASSES.CLASS_ONE },
+                { text: "Tổ 2,3", value: CLASSES.CLASS_TWO_THREE },
+                { text: "Tổ 4,5", value: CLASSES.CLASS_FOUR_FIVE },
+                { text: "Tổ giám hiệu", value: CLASSES.CLASS_PRINCIPAL },
+                { text: "Tổ chuyên biệt", value: CLASSES.CLASS_SPECIAL }
             ],
-            onFilter: (value: any, record: any) => record.team === value,
+            onFilter: (value: any, record: any) => value.includes(record.team),
             render: (team: number) => `${team}`,
         },
         {
