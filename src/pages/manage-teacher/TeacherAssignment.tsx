bc7d6e40a3d769df6329628a1f116bf4272b58e8
Juu-dev <thong.pm205029@sis.hust.edu.vn>
2025-01-26 01:58:48 +0700

    feature: teacher assigment

--- src/pages/manage-teacher/TeacherAssignment.tsx
+++ src/pages/manage-teacher/TeacherAssignment.tsx
@@ -16,11 +16,19 @@ const initialClasses = [
     { id: 1, name: 'Class 1A', teachers: [] },
     { id: 2, name: 'Class 2B', teachers: [] },
     { id: 3, name: 'Class 2C', teachers: [] },
-    { id: 3, name: 'Class 3C', teachers: [] },
-    { id: 3, name: 'Class 4C', teachers: [] },
-    { id: 3, name: 'Class 5C', teachers: [] },
-    { id: 3, name: 'Class 6C', teachers: [] },
-    { id: 3, name: 'Class 7C', teachers: [] },
+    { id: 4, name: 'Class 3C', teachers: [] },
+    { id: 5, name: 'Class 4C', teachers: [] },
+    { id: 6, name: 'Class 5C', teachers: [] },
+    { id: 7, name: 'Class 6C', teachers: [] },
+    { id: 8, name: 'Class 7C', teachers: [] },
+    { id: 11, name: 'Class 1A', teachers: [] },
+    { id: 12, name: 'Class 2B', teachers: [] },
+    { id: 13, name: 'Class 2C', teachers: [] },
+    { id: 14, name: 'Class 3C', teachers: [] },
+    { id: 15, name: 'Class 4C', teachers: [] },
+    { id: 16, name: 'Class 5C', teachers: [] },
+    { id: 17, name: 'Class 6C', teachers: [] },
+    { id: 18, name: 'Class 7C', teachers: [] },
 ];
 
 const TeacherAssignment = () => {
@@ -59,7 +67,7 @@ const TeacherAssignment = () => {
     return (
         <div className="p-4 space-y-4">
             <Row gutter={16}>
-                <Col span={8}>
+                <Col span={8} style={{ position: 'sticky', top: 0, zIndex: 10 }}>
                     <Card title="Teachers" className="bg-white shadow-md h-[90vh]">
                         {teachers.map((teacher) => (
                             <div
@@ -73,7 +81,7 @@ const TeacherAssignment = () => {
                     </Card>
                 </Col>
 
-                <Col key="class" span={16}>
+                <Col key="class" span={16} style={{ height: '90vh', overflowY: 'auto' }}>
                     <Row gutter={8}>
                         {classes.map((classItem) => (
                             <Col key={classItem.id} span={8}>
